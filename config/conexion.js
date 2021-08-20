160bacc8478eb55da46dd21ddd7f1f8007284104
Kennyd014 <david_kenny02@hotmail.com>
2021-08-19 22:12:03 -0300

    chequeado

---
+++ config/conexion.js
@@ -0,0 +1,33 @@
+const Sequelize = require('sequelize')
+
+
+let nombre_host = "localhost"
+let nombre_bd = "appnode"
+let admin_bd = "root"
+let password_bd = ""
+
+
+
+const db = new Sequelize(nombre_bd, admin_bd, password_bd, {
+    host: nombre_host,
+    dialect: 'mysql',
+    define: {
+
+        timestamps: false,
+    }
+})
+
+//verificamos la conexion
+/*db.authenticate()
+    //promise
+    .then(() => {
+        console.log('Conectados a la base de datos!')
+    })
+    .catch(err => {
+        console.log('No estamos conectados :( ' + err)
+    })*/
+
+
+
+
+module.exports = db;
