476c6ebfffbf0f979106603efb87155a1c3cd083
Kennyd014 <david_kenny02@hotmail.com>
2021-08-20 01:22:51 -0300

    update

--- config/conexion.js
+++ config/conexion.js
@@ -1,11 +1,18 @@
 const Sequelize = require('sequelize')
 
-
+//local
+/*
 let nombre_host = "localhost"
 let nombre_bd = "appnode"
 let admin_bd = "root"
 let password_bd = ""
+*/
 
+//heroku
+let nombre_host = "us-cdbr-east-04.cleardb.com"
+let nombre_bd = "heroku_850f05e705009be"
+let admin_bd = "bed42ed6083e92"
+let password_bd = "c8357e3d"
 
 
 const db = new Sequelize(nombre_bd, admin_bd, password_bd, {
